18ba559d6fd147ecc6aa312cff98079e600ab4d8
Ahmad Ajmi <ahmadajmi@outlook.com>
2014-03-22 22:22:29 +0200

    Updated to the latest version

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,68 +2,32 @@ module.exports = function(grunt){
 
 	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        htmlhint: {
-		    build: {
-		        options: {
-		            'tag-pair': true,
-		            'tagname-lowercase': true,
-		            'attr-lowercase': true,
-		            'attr-value-double-quotes': true,
-		            'doctype-first': true,
-		            'spec-char-escape': true,
-		            'id-unique': true,
-		            'head-script-disabled': true,
-		            'style-disabled': true
-		        },
-		        src: ['index.html']
-		    }
-		}
-		,
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		watch: {
-		    html: {
-		        files: ['index.html'],
-		        tasks: ['htmlhint']
-		    }
-		    ,
-		    css: {
-		        files: ['css/**/*.scss'],
-		        tasks: ['buildcss']
-		    }
+			css: {
+				files: ['css/**/*.scss'],
+				tasks: ['buildcss']
+			}
 		}
 		,
 		cssmin: {
-		    build: {
-		        src: 'css/main.css',
-		        dest: 'css/main.min.css'
-		    }
+			build: {
+				src: 'css/main.css',
+				dest: 'css/main.min.css'
+			}
 		}
 		,
 		sass: {
-		    build: {
-		        files: {
-		            'css/main.css': 'css/main.scss'
-		        }
-		    }
-		},
-		imagemin: {
-			options: {
-        progressive: true
-      },
-			dynamic: {
-				files: [{
-					expand: true,
-					cwd: 'img/',
-					src: ['**/*.{png,jpg}'],
-					dest: 'img/'
-				}]
+			build: {
+				files: {
+					'css/main.css': 'css/main.scss'
+				}
 			}
 		}
-    });
+	});
 
-    grunt.registerTask('default', []);
-    grunt.registerTask('buildcss',  ['sass', 'cssmin']);
+	grunt.registerTask('default', []);
+	grunt.registerTask('buildcss',  ['sass', 'cssmin']);
 
 };
---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+
+/* Smooth Scrolling
+* by Chris Coyier from CSS-Tricks: css-tricks.com
+*/
+$(function() {$('a[href*=#]:not([href=#])').click(function() {if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {var target = $(this.hash);target = target.length ? target : $('[name=' + this.hash.slice(1) +']');if (target.length) {$('html,body').animate({scrollTop: target.offset().top}, 500);return false;}}});});
+
+});
